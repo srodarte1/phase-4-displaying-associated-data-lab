2fda9af85086588fcda30f013ecbe49f00ca0908
+ <rodarte_sarah@yahoo.com>
2023-02-02 11:39:47 -0700

    commit tree 25a65068f7b0e52713fdbb421882968c1bcc709b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items, include: :user, status: :ok
+        # render json: items, only: [:description, :name, :price],
+        # include: [user: {only: [:username, :city]}], status: :ok
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+    def show 
+        user = User.find(params[:id])
+        render json: user, include: :items, status: :ok
 end
